08b67cede8e35d0bde22c7b605d50cf732ac9a66
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2023-06-20 16:09:48 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,14 +6,18 @@ class CPFManager:
     def __init__(self) -> None:
         self.cpf = ''
         self.cpf_log = {}
-            
+
+    
     @staticmethod    
     def _script(nove_digitos_cpf='') -> tuple:
+
         contador_regressivo_1 = 10
         resultado_digito_1 = 0
+        
         for digito in nove_digitos_cpf:
             resultado_digito_1 += int(digito) * contador_regressivo_1
             contador_regressivo_1 -= 1
+            
         digito_1 = (resultado_digito_1 * 10) % 11
         digito_1 = digito_1 if digito_1 <= 9 else 0
 
@@ -21,12 +25,13 @@ class CPFManager:
         contador_regressivo_2 = 11
 
         resultado_digito_2 = 0
+        
         for digito in dez_digitos:
             resultado_digito_2 += int(digito) * contador_regressivo_2
             contador_regressivo_2 -= 1
+            
         digito_2 = (resultado_digito_2 * 10) % 11
         digito_2 = digito_2 if digito_2 <= 9 else 0
-        
         
         return nove_digitos_cpf, digito_1, digito_2
 
@@ -42,7 +47,6 @@ class CPFManager:
         cpf_pontuado = '{}{}{}.{}{}{}.{}{}{}-{}{}'.format(*cpf_tratado)
 
         self.cpf = cpf_tratado, cpf_pontuado
-        
         self.cpf_log.update({self.cpf : 'Gerado'})
     
 
@@ -63,7 +67,6 @@ class CPFManager:
         if cpf_enviado_usuario == cpf_tratado:
             self.cpf_log.update({self.cpf : 'Verificado'})
             return True
-        
         else:
             return False
 
